100b37c1d8110233fa7a76d150fb620d4d8c4c6b
ryota N <c0b2117767@edu.teu.ac.jp>
2022-09-27 15:37:23 +0900

    クイズ作成

---
+++ ex01/quiz.py
@@ -0,0 +1,19 @@
+import random
+
+Q = ["サザエの旦那の名前は？", "カツオの妹の名前は？", "タラオはカツオから見てどんな関係？"]
+A = [["マスオ", "ますお"], ["ワカメ", "わかめ"], ["甥っ子", "おいっこ", "甥", "おい"]]
+
+num = random.randint(0,2)
+
+def quiz():
+    print("問題:" + Q[num])
+
+def ans():
+    Ans = input("回答:")
+    if Ans in A[num]:
+        print("正解！！")
+    else:
+        print("不正解！！")
+
+quiz()
+ans()
