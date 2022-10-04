6e2963072d8d408ba3f34a5460146886296a5940
ryota N <c0b2117767@edu.teu.ac.jp>
2022-10-04 16:47:06 +0900

    複数の機能を追加した

--- ex02/calc.py
+++ ex02/calc.py
@@ -13,17 +13,22 @@ def click_equal(event):
     entry.delete(0, tk.END)
     entry.insert(tk.END, res)
 
+def allclear(event):
+    entry.delete(0, tk.END)
+    
+
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("340x500")
+root.title("電卓")
 
 
 num_r = 1
 num_c = 0
 
-numbers = list(range(9, -1, -1))
-operators = ["+"]
+numbers = list(range(9, 0, -1))
+operators = [".","0","*","+","-","/"]
 for i, num in enumerate(numbers + operators, 1):
-    btn = tk.Button(root, text =f"{num}", font = ("", 30), width = "4", height = "2", command = button_click)
+    btn = tk.Button(root, text =f"{num}", font = ("", 30), width = "5", height = "1", command = button_click)
     btn.bind("<1>", button_click)
     btn.grid(row =num_r, column=num_c)
     num_c += 1
@@ -31,11 +36,15 @@ for i, num in enumerate(numbers + operators, 1):
         num_r += 1
         num_c = 0
 
-btn = tk.Button(root, text= "=", font = ("", 30), width = 4, height = 2)
+btn = tk.Button(root, text= "=", font = ("", 30), width = "5", height = "1")
 btn.bind("<1>", click_equal)
 btn.grid(row = num_r, column = num_c)
 
-entry = tk.Entry(root,  width = 10, font = (", 40"), justify = "right")
+acbtn = tk.Button(root, text = "AC", font = ("", 30), width = "5", height = "1")
+acbtn.bind("<1>", allclear)
+btn.grid(row = num_r, column = num_c)
+
+entry = tk.Entry(root, bg = "BLACK", fg = "WHITE",  width = 10, font = (", 40"), justify = "right")
 entry.grid(row = 0, column= 0,columnspan=3)
 
 root.mainloop()
