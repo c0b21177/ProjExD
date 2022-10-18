3e616f53efbe407ed4e927d59251dc2f2c6a352a
ryota N <c0b2117767@edu.teu.ac.jp>
2022-10-18 17:07:33 +0900

    機能追加済み

--- ex03/maze.py
+++ ex03/maze.py
@@ -22,27 +22,51 @@ def main_proc():
         mx -= 1
     elif key == "Right" and maze[my][mx+1] == 0:
         mx += 1
-    cx = 50 + mx * 100
-    cy = 50 + my * 100
+    cx = mx*100 + 50
+    cy = my*100 + 50
+
+    if key == "Shift_L":
+        mx = 1
+        my = 1
+
+    if mx == 13 and my == 7:
+        root.unbind("<KeyPress>")
+        root.unbind("<KeyRelease>")
 
     canvas.coords("pic", cx, cy)
     root.after(100, main_proc)
 
+def count_down():
+    global tmr
+    tmr = tmr-1
+    label["text"] = tmr
+    if tmr <= 0:
+        label["text"] = "TIME OVER"
+    elif mx == 13 and my == 7:
+        label["text"] = "GAME CLEAR"
+        return()
+    root.after(1000, count_down)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("1500x900")
+    #root.geometry("1500x900")
     root.title("迷えるこうかとん")
 
     mx, my = 1, 1
-    cx, cy = 50 + mx * 100, 50 + my * 100
+    cx, cy = mx*100 + 50, my*100 + 50
     key = ""
+    ask = ""
+    tmr = 11
 
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "BLACK")
     maze = maze_maker.make_maze(15,9)
-    print(maze)
     maze_maker.show_maze(canvas, maze)
     pic = tk.PhotoImage(file = "fig/9.png")
     canvas.create_image(cx, cy, image = pic, tag = "pic")
+    label = tk.Label(root, font = ("", 80))
+    label.pack()
+    root.after(1000, count_down)
     canvas.pack()
 
     root.bind("<KeyPress>", key_down)
